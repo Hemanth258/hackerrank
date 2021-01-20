da1ea4dcd686993907ceb25a3d484e8c2a60eff9
Hemanth258 <hemanth258147@gmail.com>
2021-01-20 18:38:12 +0530

    Update AVeryBigSum.py

--- AVeryBigSum.py
+++ AVeryBigSum.py
@@ -8,4 +8,3 @@ def aVeryBigSum(ar):
         l = l + int(i[1:])
     l = str(l)
     return str(f)+st[1:len(st)-len(l)]+str(l)
-print(aVeryBigSum([1000000001,1000000002,1000000003,1000000004,1000000005]))
