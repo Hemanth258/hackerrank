38f11270854f288fa16920ff4922095aff0107cc
Hemanth258 <hemanth258147@gmail.com>
2021-05-11 15:26:56 +0530

    Add files via upload

---
+++ tarun.py
@@ -0,0 +1,35 @@
+n = int(intput())
+s = input()
+def james(n,s):
+    m = map(list,s)
+    m = list(s)
+    print(m)
+    f = ['O']*len(s)
+    for i in range(len(s)):
+        if i == 0 and m[i] == 'c':
+            f[i] = 'I'
+            if i+1 <= len(s):
+                f[i+1] = 'I'
+            if i+2 <= len(s):
+                f[i+2] = 'I'
+        elif i == 1 and m[i] == 'c':
+            f[i] = 'I'
+            f[i-1] = 'I'
+            if i+1 <= len(s):
+                f[i+1] = 'I'
+            if i+2 <= len(s):
+                f[i+2] = 'I'
+        else:
+            if m[i] == 'c':
+                f[i] = 'I'
+                f[i-1] = 'I'
+                f[i-2] = 'I'
+                if i+1<len(s):
+                    f[i+1] = 'I'
+                if i+2<len(s):
+                    f[i+2] = 'I'
+        if m[i] == 'n' and m[i-1] != 'c' and m[i-2]!='c':
+            f[i] = 'U'
+    print(f)
+    return "".join(f)
+print(james(n,s))
