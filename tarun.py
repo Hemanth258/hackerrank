51dd41945c54c9252d4057b7f32025e0465bef94
Hemanth258 <hemanth258147@gmail.com>
2021-05-11 15:50:19 +0530

    Update tarun.py

--- tarun.py
+++ tarun.py
@@ -1,17 +1,15 @@
 n = int(input())
 s = input()
 def james(n,s):
-    m = map(list,s)
-    m = list(s)
     f = ['O']*len(s)
     for i in range(len(s)):
-        if i == 0 and m[i] == 'c':
+        if i == 0 and s[i] == 'c':
             f[i] = 'I'
             if i+1 <= len(s):
                 f[i+1] = 'I'
             if i+2 <= len(s):
                 f[i+2] = 'I'
-        elif i == 1 and m[i] == 'c':
+        elif i == 1 and s[i] == 'c':
             f[i] = 'I'
             f[i-1] = 'I'
             if i+1 <= len(s):
@@ -19,7 +17,7 @@ def james(n,s):
             if i+2 <= len(s):
                 f[i+2] = 'I'
         else:
-            if m[i] == 'c':
+            if s[i] == 'c':
                 f[i] = 'I'
                 f[i-1] = 'I'
                 f[i-2] = 'I'
@@ -27,7 +25,7 @@ def james(n,s):
                     f[i+1] = 'I'
                 if i+2<len(s):
                     f[i+2] = 'I'
-        if m[i] == 'n' and m[i-1] != 'c' and m[i-2]!='c':
+        if s[i] == 'n' and s[i-1] != 'c' and s[i-2]!='c':
             f[i] = 'U'
     return "".join(f)
 #print(james(n,s))
